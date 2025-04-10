7dd0fbc28857623ced3ff25fc7419f15fdbba8c2
eliastrana <eliastrana@gmail.com>
2025-04-10 11:59:36 +0200

    fix(frontend): favicon fix :sparkles:

--- app/layout.tsx
+++ app/layout.tsx
@@ -17,7 +17,6 @@ export default function RootLayout({
     return (
         <html lang="en">
         <head>
-            <link rel="icon" href="/favicon.ico" />
             <meta
                 name="viewport"
                 content="width=device-width, initial-scale=1.0"
